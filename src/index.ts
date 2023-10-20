87eb94e8adc23f79dde5a8569aad621ac83dd609
Rogerio Romao <58592935+rogerio-romao@users.noreply.github.com>
2023-10-20 23:57:08 +0100

    feat: typescript rules pt3
    also change esbuild script for minification and autoprefixing and sourcemap

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
-console.log('Hello Tom!');
 const e: string[] = ['a', 'b'];
-const createValue = () => 'value';
-await createValue();
-const str: string = 'foo';
-const x = 1;
-type T = number;
+const createValue = () => e;
+createValue();
 
-export { x };
-export type { T };
+interface T1 {
+    func: (_arg: unknown) => number;
+}
+export type { T1 };
+
+function foo(arg: 'bar' | 'baz') {
+    console.log(arg);
+}
+
+for (let i = 0; i < e.length; i++) {
+    // eslint-disable-next-line no-console
+    console.log(i);
+}
+
+const str = 'foo';
+str.includes('o');
